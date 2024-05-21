d24fb04bb5eab8191645498a3b486eeae4c5b36d
vanthao98 <levanthaolvt228@gmail.com>
2024-05-21 08:31:16 +0700

    show all User from github

--- src/App.js
+++ src/App.js
@@ -1,34 +1,60 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import './App.css';
-import Navbar from './component/layout/Navbar';
+import "./App.css";
+import Navbar from "./component/layout/Navbar";
 import Users from "./component/users/Users";
 
+// const App = () => {
+  
+//     const [users,setUsers] = useState([]);
+  
+//   useEffect(() =>{
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('https://api.github.com/users');
+//         console.log("GitHub users:", response.data);
+//         setUsers(response.data);
+//       } catch (error){
+//         console.error('error fetching data:',error);
+//       }
+//     };
+//     fetchData();
+//   },[]);
+//   return (
+//     <div className="App">
+//       <Navbar/>
+//       <div className='container'>
+//         <h1>
+//           Hello from react
+//         </h1>
+//       </div>
+//       <Users users={users}/>
+//     </div>
+//   );
+// }
+
+// export default App;
 const App = () => {
-  // const App = ()=> {
-    const [users,setUsers] = useState([]);
-  // }
-  useEffect(() =>{
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://api.github.com/users');
-        console.log("GitHub users:", response.data);
-      } catch (error){
-        console.error('error fetching data:',error);
+        const response = await axios.get("https://api.github.com/users");
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
     };
     fetchData();
-  },[]);
+  }, []);
   return (
     <div className="App">
-      <Navbar/>
-      <div className='container'>
-        <h1>
-          Hello from react
-        </h1>
+      <Navbar />
+      <div className="container">
+        <h1>GitHub Users Data</h1>
       </div>
+      <Users users={users} />
     </div>
-  );  
-}
-
+  );
+};
 export default App;
--- src/component/users/Users.js
+++ src/component/users/Users.js
@@ -1,18 +1,19 @@
 import React from "react";
 import UserItem from "./UserItem";
 
-const Users = (props) =>{
-    const {Users} = props;
-    const userStyle = {
-        display: "grid",
-        gridTemplateColumns: "repeat(3,1fr)", 
-        gridGap : "1rem",
-    };
-
-    return(
-        <div style={userStyle}> 
-            {Users.map((user) => (<UserItem key={user.id} user={user} />))}
-        </div>
-    );
-}
+const Users = (props) => {
+  const { users } = props;
+  const userStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(3,1fr)",
+    gridGap: "1rem",
+  };
+  return (
+    <div style={userStyle}>
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
 export default Users;
